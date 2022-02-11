dfe88b3a248aa91c89f3666afaffea777ad33b1b
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-12 01:27:31 +0300

    refactor: display person's character

--- src/components/Card/Person.tsx
+++ src/components/Card/Person.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 // Types
 import type { Person } from "../../types";
 
-export const PersonCard: React.FC<{ person: Person }> = ({ person }) => {
+export const PersonCard: React.FC<{ person: Person; personAs?: string }> = ({
+  person,
+  personAs,
+}) => {
   const imageUrl = `https://image.tmdb.org/t/p/w342${person.profile_path}`;
 
   return (
@@ -14,9 +17,13 @@ export const PersonCard: React.FC<{ person: Person }> = ({ person }) => {
           backgroundImage: `url('${imageUrl}')`,
         }}
       >
-        <div className="inset-0 items-center justify-center hidden h-full transition-all rounded-md group-hover:flex bg-black/40">
-          <h3 className="px-2 text-xl font-medium leading-none text-center text-white">
+        <div className="inset-0 items-center justify-center hidden h-full transition-all rounded-md group-hover:flex bg-black/60">
+          <h3
+            className={`px-2 text-xl font-medium leading-none text-center
+            ${personAs ? "text-white/70" : "text-white"}`}
+          >
             {person.name}
+            {personAs && <span className="ml-1 text-white">as {personAs}</span>}
           </h3>
         </div>
       </a>
