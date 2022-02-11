168a8b2e7f8e052538278823349e4aa118dec281
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-12 01:11:37 +0300

    refactor: return more data and add actor handler

--- src/structures/handler.ts
+++ src/structures/handler.ts
@@ -3,6 +3,23 @@ import axios from "axios";
 // Types
 import { TmdbApiResponse } from "../types";
 
+interface ExtendedMovieInfo extends MovieInfo {
+  media_type?: "movie" | "tv";
+  credits?: {
+    cast: ActorInfo[];
+    crew: ActorInfo[];
+  };
+  videos: {
+    results: Video[];
+  };
+}
+
+interface ExtendedActorInfo extends ActorInfo {
+  combined_credits: {
+    cast: ExtendedMovieInfo[];
+    crew: ExtendedMovieInfo[];
+  };
+}
 class TmdbHandler {
   apiKey = process.env.TMDB_API_KEY;
 
@@ -26,7 +43,42 @@ class TmdbHandler {
   }
 
   async getInfo(movieId: string, type: "movie" | "tv" = "movie") {
-    return this.fetch(`/${type}/${movieId}`);
+    const { videos, credits, ...data } = await this.fetch<ExtendedMovieInfo>(
+      `/${type}/${movieId}?append_to_response=videos,credits`
+    );
+
+    const latestTrailer = [...videos?.results]
+      ?.filter((video) => video.type === "Trailer")
+      ?.sort(
+        (a: Video, b: Video) =>
+          new Date(b.published_at).getTime() -
+          new Date(a.published_at).getTime()
+      )?.[0];
+
+    const sortedCast = [...(credits?.cast || [])].sort(
+      (a, b) => b.popularity - a.popularity
+    );
+
+    return {
+      ...data,
+      trailer: latestTrailer,
+      cast: sortedCast.slice(0, 6),
+    };
+  }
+
+  async getActor(actorId: string) {
+    const { combined_credits, ...data } = await this.fetch<ExtendedActorInfo>(
+      `/person/${actorId}?append_to_response=combined_credits`
+    );
+
+    const sortedCast = [...combined_credits?.cast]
+      .filter((item) => item.media_type === "movie")
+      .sort((a, b) => b.popularity - a.popularity);
+
+    return {
+      ...data,
+      cast: sortedCast?.slice(0, 6) || [],
+    };
   }
 
   async getDiscovery() {
