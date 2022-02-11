be67ae6832017ee01f6fe00c0643f27e3111caae
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-11 03:42:07 +0300

    refactor: featured movie card loader too

--- src/components/Card/Loader.tsx
+++ src/components/Card/Loader.tsx
@@ -1,18 +1,26 @@
-export const Loader: React.FC<{ count?: number }> = ({ count = 1 }) => {
-  if (count === 1) return <Card />;
+export const Loader: React.FC<{
+  count?: number;
+  type?: "card" | "card-large";
+}> = ({ count = 1, type = "card" }) => {
+  const SelectedCard = type === "card-large" ? CardLarge : Card;
 
+  if (count === 1) return <SelectedCard />;
   return (
     <>
       {Array(count)
         .fill(null)
         .map((_, idx) => (
-          <Card key={idx} />
+          <SelectedCard key={idx} />
         ))}
     </>
   );
 };
 
-const Card: React.FC = () => (
+export const Card: React.FC = () => (
+  <div className="w-full bg-gray-200 rounded-md animate-pulse h-80" />
+);
+
+export const CardLarge: React.FC = () => (
   <div className="w-full bg-gray-200 rounded-md animate-pulse h-80" />
 );
 
