01864281c50a93a79149c5d4a2b3ec3473bf6458
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-11 03:41:18 +0300

    feat: get days left

---
+++ src/functions/getDaysDaysLeft.ts
@@ -0,0 +1,10 @@
+export const getDaysDaysLeft = (date: string) => {
+  const today = new Date();
+  const nextEpisodeDate = new Date(date);
+
+  return Math.ceil(
+    (nextEpisodeDate.getTime() - today.getTime()) / (1000 * 3600 * 24)
+  );
+};
+
+export default getDaysDaysLeft;
