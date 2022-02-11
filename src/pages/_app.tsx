44aa64c4c4719e424f2ed62ce508a47ee29b0da2
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-11 03:40:57 +0300

    refactor: make use of nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import Router from "next/router";
+
+// NProgress
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 // Import Tailwind CSS
 import "../styles/tailwind.css";
@@ -14,6 +19,16 @@ const meta = {
   title: "Movie App",
   description: "Very good movie app.",
 };
+
+// NProgress settings and events
+NProgress.configure({
+  showSpinner: true,
+  parent: "body",
+});
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done(true));
+Router.events.on("routeChangeError", () => NProgress.done(true));
 
 function MovieApp({ Component, pageProps }: AppProps) {
   return (
@@ -33,7 +48,7 @@ function MovieApp({ Component, pageProps }: AppProps) {
         <meta name="twitter:description" content={meta.description} />
       </Head>
 
-      <div className="min-h-screen bg-gray-100/90 backdrop-blur-md backdrop-filter">
+      <div className="min-h-screen bg-white/90 backdrop-blur-md backdrop-filter">
         <Navbar />
 
         <div className="container py-10 mx-auto">
@@ -43,5 +58,12 @@ function MovieApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+// TODO Footer
+// TODO Actor pages
+// TODO Better landing hero
+// TODO Search, categories and stuff
+// TODO Responsiveness
+// TODO Studio pages
 
 export default MovieApp;
