5baa28665abc6ee572ca503a21e78e23eb19c2ff
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-11 03:41:47 +0300

    feat: get genre emoji icon

---
+++ src/functions/getGenreEmoji.ts
@@ -0,0 +1,47 @@
+/* 😻 github copilot */
+export const getGenreEmoji = (genre: string): string => {
+  switch (genre) {
+    case "Action":
+      return "🗡️";
+    case "Action & Adventure":
+      return "🗡️";
+
+    case "Adventure":
+      return "🗺️";
+    case "Animation":
+      return "🎭";
+
+    case "Adult":
+      return "🔞";
+
+    case "Sci-Fi":
+      return "🔬";
+    case "Sci-Fi & Fantasy":
+      return "🔬";
+
+    case "Comedy":
+      return "🎭";
+    case "Drama":
+      return "🎬";
+    case "Fantasy":
+      return "🧙‍♂️";
+    case "Historical":
+      return "🏛️";
+    case "Horror":
+      return "🧙‍♀️";
+    case "Mystery":
+      return "🕵️‍♀️";
+    case "Romance":
+      return "💑";
+    case "Science Fiction":
+      return "🧑‍🦯";
+    case "Thriller":
+      return "🧙‍♂️";
+    case "Western":
+      return "🗡️";
+    default:
+      return "🤷‍♂️";
+  }
+};
+
+export default getGenreEmoji;
