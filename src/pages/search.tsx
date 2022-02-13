a8f5be023ff5678ef333e0927540d651c24e2a14
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-13 03:26:32 +0300

    refactor: search page improvements

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -100,19 +100,19 @@ const SearchPage: NextPage = () => {
   }, [data, sortValue]);
 
   return (
-    <div className="space-y-24">
-      <div className="space-y-4">
-        <h1 className="text-4xl font-semibold text-gray-800">Search</h1>
+    <>
+      <div className="absolute inset-0 shadow-md bg-gradient-to-r from-brand-dark-blue to-brand-blue h-80 -z-10" />
 
-        <div className="grid w-full gap-14 md:grid-cols-2">
-          <SearchInput value={searchValue} setValue={setValue} />
+      <div className="px-6 space-y-24 md:px-0">
+        <div className="py-6 space-y-4">
+          <h1 className="text-4xl font-semibold text-center text-white md:text-left">
+            Search
+          </h1>
 
-          <div className="flex items-center justify-end w-full space-x-2">
-            <label htmlFor="sort" className="text-gray-400">
-              Sort
-            </label>
+          <div className="grid w-full text-black/70 gap-x-14 gap-y-6 md:grid-cols-2">
+            <SearchInput value={searchValue} setValue={setValue} />
 
-            <div className="w-2/6">
+            <div className="w-2/3 ml-auto md:w-2/6">
               <Select
                 placeholder="Select an option"
                 value={sortValue}
@@ -122,50 +122,50 @@ const SearchPage: NextPage = () => {
             </div>
           </div>
         </div>
-      </div>
 
-      {!loading && getSortedValues.noResults && (
-        <div>Nothing found with that query.</div>
-      )}
+        {!loading && getSortedValues.noResults && (
+          <div className="text-center">Nothing found with that query.</div>
+        )}
 
-      {loading ? (
-        new Array(3).fill(0).map((_, i) => (
-          <div key={i} className="space-y-6">
-            <div className="w-1/6 h-8 bg-gray-200 rounded-md animate-pulse" />
+        {loading ? (
+          new Array(3).fill(0).map((_, i) => (
+            <div key={i} className="space-y-6">
+              <div className="w-1/6 h-8 bg-gray-200 rounded-md animate-pulse" />
 
-            <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
-              <Loader count={6} />
+              <div className="grid grid-cols-2 gap-4 md:grid-cols-6">
+                <Loader count={6} />
+              </div>
             </div>
+          ))
+        ) : (
+          <div className="space-y-24">
+            {!!getSortedValues.movies.length && (
+              <Item title="Movies" length={getSortedValues.movies.length}>
+                {getSortedValues.movies.map((item) => (
+                  <MovieCard key={item.id} movie={item} />
+                ))}
+              </Item>
+            )}
+
+            {!!getSortedValues.series.length && (
+              <Item title="Series" length={getSortedValues.series.length}>
+                {getSortedValues.series.map((item) => (
+                  <MovieCard key={item.id} movie={item} type="tv" />
+                ))}
+              </Item>
+            )}
+
+            {!!getSortedValues.people.length && (
+              <Item title="Actors" length={getSortedValues.people.length}>
+                {getSortedValues.people.map((item) => (
+                  <PersonCard key={item.id} person={item} />
+                ))}
+              </Item>
+            )}
           </div>
-        ))
-      ) : (
-        <div className="space-y-24">
-          {!!getSortedValues.movies.length && (
-            <Item title="Movies" length={getSortedValues.movies.length}>
-              {getSortedValues.movies.map((item) => (
-                <MovieCard key={item.id} movie={item} />
-              ))}
-            </Item>
-          )}
-
-          {!!getSortedValues.series.length && (
-            <Item title="Series" length={getSortedValues.series.length}>
-              {getSortedValues.series.map((item) => (
-                <MovieCard key={item.id} movie={item} type="tv" />
-              ))}
-            </Item>
-          )}
-
-          {!!getSortedValues.people.length && (
-            <Item title="Actors" length={getSortedValues.people.length}>
-              {getSortedValues.people.map((item) => (
-                <PersonCard key={item.id} person={item} />
-              ))}
-            </Item>
-          )}
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
@@ -173,12 +173,16 @@ export const SearchInput: React.FC<{
   value?: string;
   setValue?: (val: string) => void;
 }> = ({ value, setValue }) => {
-  const { push } = useRouter();
+  const { route, push } = useRouter();
 
   const props = {
     placeholder: "Search for movies, series or actors...",
-    className:
-      "w-full px-6 py-2 transition-colors rounded-md outline-none appearance-none placeholder-current bg-gray-200/40 truncate focus:bg-gray-200/50 hover:bg-gray-200/40",
+    className: `w-full px-6 py-2 backdrop-blur-sm transition-colors rounded-md outline-none appearance-none placeholder-current  truncate
+    ${
+      route === "/search"
+        ? "bg-gray-200 hover:bg-gray-300 focus:bg-gray-300"
+        : "bg-gray-200/20 focus:bg-gray-200/50 hover:bg-gray-200/40"
+    }`,
   };
 
   if (!setValue)
